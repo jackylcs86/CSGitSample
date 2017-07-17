86cd9334a889a7beb92056afadff426e29cd6bd5
sifb <sifb@sifbs-MacBook-Pro-2.local>
2017-07-17 17:22:42 +0800

    Commit only Appdelegate

--- CSGitSample/AppDelegate.m
+++ CSGitSample/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Just to modify sth else...
     return YES;
 }
 
