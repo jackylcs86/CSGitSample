c631261cdae96d685cc5b544f5d80bf4e0693756
jackylcs86 <Jackylcs@KCs-MacBook-Pro.local>
2018-03-06 12:06:07 +0800

    HisModel Change

---
+++ CSGitSample/HisModel.h
@@ -0,0 +1,13 @@
+//
+//  HisModel.h
+//  CSGitSample
+//
+//  Created by Jackylcs on 06/03/2018.
+//  Copyright © 2018 sifb. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface HisModel : JSONModel
+
+@end
---
+++ CSGitSample/HisModel.m
@@ -0,0 +1,13 @@
+//
+//  HisModel.m
+//  CSGitSample
+//
+//  Created by Jackylcs on 06/03/2018.
+//  Copyright © 2018 sifb. All rights reserved.
+//
+
+#import "HisModel.h"
+
+@implementation HisModel
+
+@end
